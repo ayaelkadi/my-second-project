83e6224087abf26859666b71e4f9775c3108dde9
ayaelkadi <elkadiaya00@gmail.com>
2025-11-10 11:59:04 +0100

    malika

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const ARTICLE_PER_PAGE = 6;
 
 const PRODUCTION_DOMAIN = "https://my-second-project-zigj.vercel.app/";
-const DEVELOPMENT_DOMAIN = "http://localhost:3003";
+const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
 export const DOMAIN = process.env.NODE_ENV === 'production' 
     ? PRODUCTION_DOMAIN
